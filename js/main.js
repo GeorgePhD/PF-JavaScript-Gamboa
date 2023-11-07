c8039d6d0d1499bb7afa3189bc9cd3f74ae0f795
George <lets.devel0pme@gmail.com>
2023-11-07 11:28:44 -0300

    update: sections search and index done, contact is missing the validating part

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@ const changeMode = document.querySelector('#change-mode');
 const body = document.body;
 const icon = document.querySelectorAll('.icon');
 
-
-
 const setUserModePreference = (mode) => {
     localStorage.setItem('userMode', mode);
 };
@@ -23,12 +21,12 @@ const updateUIForUserModePreference = () => {
     } else {
         body.classList.remove('dark-mode');
     }
-    //changeIcons(userMode);
+
 };
 
 const changeModes = () => {
     console.log('Mode changed');
-    
+
     if (body.classList.contains('dark-mode')) {
         body.classList.remove('dark-mode');
         body.classList.add('light-mode');
@@ -49,41 +47,47 @@ updateUIForUserModePreference();
 /* home cards start here */
 const url = 'https://rickandmortyapi.com/api/character';
 
+let data;
 fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
+    .then((response) => response.json())
+    .then((responseData) => {
+        data = responseData;
         console.log(data);
-        data.results.forEach((result) => {
 
-            const card = document.createElement('div');
-            card.classList.add('card');
+        if (data.results) {
+            data.results.forEach((result) => {
+                const card = document.createElement('div');
+                card.classList.add('card');
 
-            // Create and populate elements for character information
-            const name = document.createElement('h2');
-            name.textContent = result.name;
+                // Create and populate elements for character information
+                const name = document.createElement('h2');
+                name.textContent = result.name;
 
-            const status = document.createElement('p');
-            status.textContent = `Status: ${result.status}`;
+                const status = document.createElement('p');
+                status.textContent = `Status: ${result.status}`;
 
-            const species = document.createElement('p');
-            species.textContent = `Species: ${result.species}`;
+                const species = document.createElement('p');
+                species.textContent = `Species: ${result.species}`;
 
-            const image = document.createElement('img');
-            image.src = result.image;
-            
+                const image = document.createElement('img');
+                image.src = result.image;
 
-            // Append the elements to the card
-            card.appendChild(name);
-            card.appendChild(status);
-            card.appendChild(species);
-            card.appendChild(image);
+                const container = document.getElementById('character-container');
 
-            // Append the card to a container element (e.g., a div with the id "character-container")
-            const container = document.getElementById('character-container');
-            container.appendChild(card);
-        });
+                if (container) {
+                    // Append the elements to the card
+                    card.append(name);
+                    card.append(status);
+                    card.append(species);
+                    card.append(image);
+
+                    // Append the card to a container element
+                    container.append(card);
+                }
+            });
+        } else {
+            console.error('No results found in the data.');
+        }
     })
     .catch((error) => {
         console.error('Error:', error);
@@ -93,8 +97,39 @@ fetch(url)
 
 
 /* search section starts here */
-const searchDisplay = document.querySelector('#search-display').value;
+const userSearch = document.querySelector('#search');
+console.log(userSearch);
+const displayButton = document.querySelector('#displayBtn');
+console.log(displayButton);
+const searchDisplay = document.querySelector('#search-display');
 console.log(searchDisplay);
+const addToFavoritesButton = document.querySelector('#add-to-favorites');
+
+displayButton.addEventListener('click', () => {
+
+    const searchName = userSearch.value.toLowerCase();
+    const result = data.results.find((character) => character.name.toLowerCase() === searchName);
+
+    if (result) {
+
+        searchDisplay.innerHTML = `
+        <div class='search-container'>
+        <h3 class='search-subtitle'>character found:</h3>
+        <p class='search-name'>${result.name}</p>
+        <p class='search-status'>Status: ${result.status}</p>
+        <p class='search-species'>Species: ${result.species}</p>
+        <button class='add-to-favorites' id='add-to-favorites' title='add to favorites, it will be shown in the favorites section'><i class="fa-solid fa-heart"></i></button>
+        <img class='search-img' src=${result.image}>
+        </div>
+        `;
+
+    } else {
+
+        searchDisplay.innerHTML = `<p class='search-error'>Character not found.</p>`;
+    }
+
+})
+
 /* search section ends here */
 
 
